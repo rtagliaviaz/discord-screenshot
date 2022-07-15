fbabbdc86ccecad7d3f7c8a54e45c010583a013b
unknown <rtagliavia.pp@gmail.com>
2022-07-15 16:17:40 -0400

    discord screenshot bot

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
-const axios = require("axios");
 const Discord = require("discord.js");
-const express = require('express');
 const fs = require('fs');
 
 require("dotenv").config();
 
 
-//set path to public
-
-// app.use('/public', express.static(path.join(__dirname, 'public')));
-
-const publicPath = path.join(__dirname, 'public');
-
 const imagesPath = path.join(__dirname, 'images')
 
-//if imagees doesn't exist then create it
+
 if (!fs.existsSync(imagesPath)) {
     fs.mkdirSync(imagesPath);
 }
@@ -36,20 +28,16 @@ const client = new Discord.Client({
 
 
 client.on("ready", async () => {
-  // console.log(msg)
   console.log(`Logged in as ${client.user.tag}!`);
   let channel = client.channels.cache.get('855537249155547199')
-// console.log(channel)
 
-// channel.send("Hello World")
+
   await getScreenhot();
   const embed2 = new Discord.MessageEmbed()
     .setTitle("My Screenshot")
     .setImage(`attachment://dashboard.png`)
     
-  // channel.send({ embeds: [embed2] });
   channel.send({ embeds: [embed2], files: [`${imagesPath}/dashboard.png`] });
-
   
 });
 
@@ -68,24 +56,13 @@ const getScreenhot = async () => {
   await page.goto('https://dev.to/rtagliavia')
   await page.screenshot({ path: `${imagesPath}/dashboard.png` })
 
-  //if saved to imagesPath/google.png
   if (fs.existsSync(`${imagesPath}/dashboard.png`)) {
     console.log("Screenshot saved");
   }
 
-  
-  
   await browser.close()
 
-  //wait until the image is saved and send it
-  
-
-  
 }
-
-
-//send the screenshot to discord channel every day at 8:00 pm
-
 
 
 
